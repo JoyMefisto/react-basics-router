af4bd64dd099608324214011611816312124155f
Shanin Artem <mefistoggc@mail.ru>
2017-06-05 00:12:51 +0300

    browser history

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, IndexRoute, IndexRedirect, hashHistory } from 'react-router';
+import { Router, Route, IndexRoute, IndexRedirect, browserHistory } from 'react-router';
+
+import books from './data/books';
 
 import App from './App';
 import Home from './components/Home';
@@ -11,15 +13,15 @@ import NotFound from './components/NotFound';
 import Shelf from './components/Shelf';
 
 ReactDOM.render(
-    <Router history={hashHistory} >
+    <Router history={browserHistory} >
         <Route path="/" component={App}>
             <IndexRoute component={Home} />
             <Route path="about" component={About} />
             <Route path="books" component={Books}>
                 <IndexRedirect to="/books/javascript" />
                 <Route path=":topic">
-                    <IndexRoute component={Shelf} />
-                    <Route path=":slug" component={Book} />
+                    <IndexRoute component={Shelf} books={books} />
+                    <Route path=":slug" component={Book} books={books} />
                 </Route>
             </Route>
             <Route path="*" component={NotFound} />
